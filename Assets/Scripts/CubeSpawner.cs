76aad396af16c6456fbdfe9d4168d8bbe03c09fe
Robert Dalton Zautke <rzautke@umich.edu>
2015-01-30 13:16:46 -0500

    Initial Commit.

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour {
+
+    public GameObject spawnerCube;
+
+    public int maxCubes = 100;
+    public int currentNumberOfCubes;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+       // while (currentNumberOfCubes < maxCubes) {
+            GameObject.Instantiate(spawnerCube);
+        //    currentNumberOfCubes++;
+        //}
+
+	}
+}
